af6467a7652c6d7cb0fb133071ebdf542025bdfc
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2019-09-30 08:36:51 +0100

    Update circularQueue.cpp

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -1,4 +1,56 @@
+//2 solutions 
 
+class MyCircularQueue {
+private: 
+    int size = 0;
+    const int N; 
+    vector<int> v; 
+    int begin = 0; 
+    int end = 0; 
+public:
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    MyCircularQueue(int k): N(k), v(k,0) {}
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    bool enQueue(int value) {
+        if (isFull()) return false; 
+        v[end] = value; 
+        end = (end+1)%N; 
+        size++; 
+        return true; 
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    bool deQueue() {
+        if (isEmpty())
+            return false; 
+        begin = (begin+1)%N;
+        size--; 
+        return true; 
+    }
+    
+    /** Get the front item from the queue. */
+    int Front() {
+        return isEmpty()? -1: v[begin]; 
+    }
+    
+    /** Get the last item from the queue. */
+    int Rear() {
+        return isEmpty()? -1: v[(end+N-1)%N]; 
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    bool isEmpty() {
+        return !size; 
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    bool isFull() {
+        return (size == N); 
+    }
+};
+
+//-------------------------------------------------------------------------------------------------
 class MyCircularQueue {
 private:
     int tail;
