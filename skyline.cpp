03acaa4785b165b8d7a0f4f82b28cd7de4729dbd
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-01-22 16:04:57 +0000

    finished brute force

--- skyline.cpp
+++ skyline.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std; 
 
+//this is no a good solution!
+//it's basically brute force
 
-int main(){
-    vector<vector<int>> grid{{3, 0, 8, 4}, 
-                             {2, 4, 5, 7},
-                             {9, 2, 6, 3},
-                             {0, 3, 1, 0} }; 
-
-    int size = grid.size(); 
-    //getting the max for each line
-    vector<int> max_line;
-    for (int i = 0; i < size; i++)
-        auto max = max_element(grid[i].begin(), grid[i].end()); 
-        cout << max << endl; 
-        //max_line.push_back(max); 
+//APROACH 1: 
+class Solution {
+public:
     
-    //changing all the elements
-    /**
-    for (int i = 0; i < size; i++){
-        grid[0] = vector<int>(size, max_line[i]); 
+    int find_max(vector<int> v){
+        int max = 0; 
+        for (int n:v)
+            max = n>max? n: max; 
+        return max;   
     }
-    for (vector<int> v1: grid){
-        cout << endl; 
-        for (int v2: v1)
-            cout << v2  << " "; 
+
+    int find_max_col(vector<vector<int>> v, int col, int size){
+        int max = 0; 
+        for (int i= 0 ; i < size; i++)
+            max = v[i][col] > max? v[i][col]: max; 
+        return max; 
     }
-    return 0; 
-    **/
-}
+    
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int size = grid.size(); 
+
+        int max = 0, maxcol; 
+        //getting the max for each line
+        vector<int> max_line;
+        vector<int> max_col; 
+        for (int i = 0; i < size; i++){
+            max = find_max(grid[i]);  
+            max_line.push_back(max); 
+            maxcol = find_max_col(grid, i, size); 
+            max_col.push_back(maxcol); 
+        }
+
+
+        //changing all the elements for the maximum
+        int soma = 0; 
+        for (int line = 0; line < size; line++){
+            for (int col = 0; col < size; col ++){
+                if (grid[line][col] != max_line[line] && grid[line][col] != max_col[col]) 
+                    soma += min(max_line[line], max_col[col]) -grid[line][col]; 
+                
+        } 
+        }
+        return soma; 
+    }
+};
+
