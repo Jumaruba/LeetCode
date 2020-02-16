361beca07c1fbbc6cbfe827cd428912cce17c446
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-02-16 19:09:12 +0000

    Create climbing_stairs.cpp

---
+++ climbing_stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0) return 0; 
+        if (n == 1) return 1; 
+        vector <int> vec(n, 0); 
+        vec[0] = 1; 
+        vec[1] = 2; 
+        for (int i = 2; i< n; i++){
+            vec[i] = vec[i-1] + vec[i-2]; 
+        }
+        return vec[n-1]; 
+    }
+};
