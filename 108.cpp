a8fe1d06ba13951cbf6cee603d27bb44aade8ab3
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-07-20 21:35:57 +0100

    Update 108.cpp

--- 108.cpp
+++ 108.cpp
@@ -26,3 +26,28 @@ public:
     }
     
 };
+//Even better to use iterators 
+
+/*
+class Solution {
+public:
+    using iter = vector<int>::const_iterator;
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.empty()) return nullptr;
+        
+        return buildBST(nums.begin(), nums.end());
+    }
+    
+    TreeNode* buildBST(iter b, iter e) {
+        if (b >= e) return nullptr;
+        
+        iter m = b + (e - b) / 2;
+        
+        TreeNode* node = new TreeNode(*m);
+        node->left = buildBST(b, m);
+        node->right = buildBST(m + 1, e);
+        
+        return node;
+    }
+};
+*/ 
