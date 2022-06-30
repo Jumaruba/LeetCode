408e58dae2b22d4a1279182eaf19b7bd718659fd
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-06-30 09:37:31 +0100

    Create 724.py

---
+++ 724.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        right = sum(nums)
+        left = 0 
+        for i,n in enumerate(nums):
+            right -= n
+            if left == right:
+                return i
+            left += n 
+            
+            
+        return -1
