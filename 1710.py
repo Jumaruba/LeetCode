b5f8cec1103fa38062ce10bb2ec3e8b3c80d509d
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-07-01 08:31:52 +0100

    Create 1710.py

---
+++ 1710.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumUnits(self, box_types: List[List[int]], truck_size: int) -> int:
+        sorted_box_types = sorted(box_types, key=lambda box: -box[1])
+        total_elements = 0
+        for index, (num_boxes, num_elements) in enumerate(sorted_box_types):
+            new_truck_size = max(truck_size - num_boxes, 0)
+            used_boxes = truck_size - new_truck_size 
+            truck_size = new_truck_size
+            total_elements += used_boxes*num_elements
+        return total_elements
+            
