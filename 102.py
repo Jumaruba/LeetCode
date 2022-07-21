da4d1f33aa82c23faabc3d9af0753ee3863de07e
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-07-21 12:52:23 +0100

    Update 102.py

--- 102.py
+++ 102.py
@@ -26,4 +26,35 @@ class Solution:
             
         recursive(root, 0)
         return levels
+    
+# =============================
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = [] 
+        next_level = []
+        if root != None: 
+            next_level = [root]
+        curr_level = [] 
         
+        
+        while next_level: 
+            curr_level = next_level.copy()
+            next_level = []
+            
+            if curr_level:
+                levels.append([])
+                
+            for node in curr_level:
+                levels[-1].append(node.val)
+                if node.left != None: 
+                    next_level.append(node.left)
+                if node.right != None: 
+                    next_level.append(node.right)
+        return levels 
+            
