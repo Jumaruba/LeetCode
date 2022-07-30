09c8d5c5ada689bd157334fe29a5ab1f36491537
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-07-30 19:10:03 +0100

    Update 916.py

--- 916.py
+++ 916.py
@@ -37,3 +37,29 @@ class Solution:
         for i in word:
             h[i]+=1
         return h
+
+    
+# Simplified solution using python functions:
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        result = []
+        self.h2 = self.compileWords(words2)
+        for w1 in words1: 
+            h1 = Counter(w1)
+            if self.isUniversal(h1):
+                result.append(w1)
+            
+        return result 
+        
+    def isUniversal(self, h1):
+        for l,n in self.h2.items():
+            if h1[l] == '' or self.h2[l] > h1[l]:
+                return False
+        return True 
+    
+    def compileWords(self, words):
+        h = collections.defaultdict(int)
+        for h_curr in map(Counter, words):
+            for l, value in h_curr.items():
+                h[l] = max(value, h[l])
+        return h
