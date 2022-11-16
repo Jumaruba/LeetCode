27e503370e24bece83a21680beaa869aa1418bba
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-11-16 09:47:01 +0000

    Update 222.java

--- 222.java
+++ 222.java
@@ -86,3 +86,26 @@ An also interesting solution in python
         return count(root)
 
 */ 
+
+/* Another simple solution in java with explanation here: 
+https://jumaruba.github.io/_pages/others/CP/222-count-complete-tree-node.html
+
+
+class Solution 
+{
+    public int countNodes(TreeNode root) 
+    {
+        if (root == null) return 0;
+
+        TreeNode left = root, right = root;
+        int l = 1, r = 1;
+        
+        while ((left  = left.left)   != null) ++l;
+        while ((right = right.right) != null) ++r;
+        
+        if (l == r) return (1 << l) - 1;
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
+*/ 
