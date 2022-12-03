5858bfaf9e7011d5a7b5dc1ec91bd94c446282e9
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-12-03 14:40:45 +0000

    Update 451.cpp

--- 451.cpp
+++ 451.cpp
@@ -20,3 +20,56 @@ public:
         return ans; 
     }
 };
+
+
+// Another terrible soltuion 
+
+class Ch {
+    public: 
+    string s; 
+    int times; 
+    char c; 
+    Ch(char c){
+        times = 1; 
+        s = ""; 
+        s.push_back(c);
+        this->c = c; 
+    }
+
+    void inc(){
+        times++; 
+        s.push_back(c); 
+    }
+
+    friend bool operator<(const Ch& ch1, const Ch& ch2){
+        return ch1.times < ch2.times;
+    }
+
+};
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, Ch> map; 
+        string res(""); 
+        for (char c: s){
+            if (map.find(c) == map.end()){
+                Ch m(c); 
+                map.insert({c, m}); 
+            } else {
+                map.at(c).inc();
+            }
+        }
+
+        priority_queue<Ch> pq; 
+        for (auto& it: map){
+            pq.push(it.second);  
+        }
+    	
+        while(!pq.empty()){
+            res = res + pq.top().s; 
+            pq.pop();
+        }
+        return res ;
+    }
+};
