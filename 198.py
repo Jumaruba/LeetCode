901efaa0a89ec4f705b6a0384330509ad7f15c71
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-12-14 10:30:25 +0000

    Update 198.py

--- 198.py
+++ 198.py
@@ -14,3 +14,21 @@ class Solution:
             prev = curr 
             
         return max(prev, prevprev) 
+
+    
+# Another solution 14/12/2022
+
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+        nums_size = len(nums)
+        if nums_size <= 2:
+            return max(nums)
+
+        arr = [nums[0],nums[1],nums[0]+nums[2]]
+        for i in range(3, nums_size):
+            val = max(arr[0], arr[1]) + nums[i]
+            arr[0] = arr[1]
+            arr[1] = arr[2]
+            arr[2] = val
+        return max(arr
