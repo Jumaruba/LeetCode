974bdc922df908adabb5ee7c726428a18253b593
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-12-17 17:03:22 +0000

    Update 020.cpp

--- 020.cpp
+++ 020.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // 1 = (; -1 = ) 
-        // 2 = [; -2 = ] 
-        // 3 = {; -3 = }
-        if (s.size() == 0) return true; 
-        stack<int> st; 
-        for (int i = 0 ; i < s.size() ; i++){
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{' ) st.push(s[i]); 
-            else{
+        stack<char> st = stack<char>(); 
+        char aux; 
+        for (char c: s){
+            if (c == '(') st.push(')'); 
+            else if (c == '[') st.push(']');
+            else if (c == '{') st.push('}');
+            else {
                 if (st.empty()) return false; 
-                char bracket = st.top(); 
+                aux = st.top(); 
                 st.pop(); 
-                if (s[i] == ')' && bracket != '(') return false; 
-                else if (s[i] == ']' &&  bracket != '[') return false; 
-                else if (s[i] == '}' && bracket != '{') return false; 
-                
+                if (aux != c) return false; 
             }
         }
-        return st.empty() ? true: false; 
+        return st.empty(); 
     }
+
 };
