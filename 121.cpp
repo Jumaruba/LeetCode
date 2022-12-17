5b9eb4e263cf9375e3d678c05a0ea52c77e5d2bc
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-12-17 19:07:20 +0000

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if (prices.empty()) return 0;
-        int lowest = prices[0]; 
-        int solution = 0; 
-        for (int i = 1; i < prices.size(); i++){
-            if (prices[i] - lowest > solution) solution = prices[i] - lowest; 
-            if (prices[i] < lowest) lowest = prices[i]; 
-        }
-        return solution; 
+        int m = 0, buy = prices[0]; 
+        for (int i =  1; i < prices.size(); i++) { 
+            if (prices[i] < buy)  buy = prices[i]; 
+            m = max(prices[i] - buy, m); 
+        } 
+
+        return m; 
     }
-};
+}
