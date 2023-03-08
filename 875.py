e97144fd3bc899d687755c17af1cdee90c82d2ce
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2023-03-08 16:24:17 +0000

    Update 875.py

--- 875.py
+++ 875.py
@@ -3,22 +3,17 @@ class Solution:
         if len(piles) == 1:
             return int(ceil(piles[0]/h))
 
-        def get_hours(k: int):
-            return sum(list(map(lambda x: ceil(x/k), piles)))
+        def can_eat(k: int):
+            return sum(list(map(lambda x: ceil(x/k), piles))) <= h
 
         mx = max(piles)
         mn = 1
-        m = ceil((mx-mn)/2 + mn)
-        while mn+1 < mx: 
-            m = ceil((mx-mn)/2 + mn)
-            curr_h = get_hours(m)
-            if curr_h <= h:
+        while mn < mx: 
+            m = (mx-mn)//2 + mn
+            ce = can_eat(m)
+            if ce:
                 mx = m
-            elif curr_h > h:
+            else:
                 mn = m+1
 
-        h_mn = get_hours(mn)
-        h_mx = get_hours(mx)
-        if h_mn <= h:
-            return mn
-        return mx
+        return mn
